7537474702e8d3a468918895f4794fa449829971
George Perry <george.e.perry5@gmail.com>
2022-09-01 17:45:01 -0700

    reviews correctly showing up on other profiles if the resort names match

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,20 +2,31 @@ const Resort = require('../models/resort')
 
 function create (req, res) {
   // Find the movie to embed the review within
-  Resort.findById(req.params.id, function (err, resort) {
-    // Add the user-centric info to req.body (the new review)
-    req.body.user = req.user._id
-    req.body.userName = req.user.name
-    req.body.userAvatar = req.user.avatar
+  console.log('this is req.params.id', req.params.id)
+  Resort.findById(req.params.id, function (err, targetResort) {
+    resortName = targetResort.resortName
+    console.log('resortName', resortName)
+    Resort.find({ resortName: resortName }, function (err, resort) {
+      // Add the user-centric info to req.body (the new review)
+      console.log('all of the resorts with the name inputed', resort)
 
-    // Push the subdoc for the review
-    resort.reviews.push(req.body)
-    // Always save the top-level document (not subdocs)
-    review.save(function (err) {
-      res.redirect(`/resorts/${resort._id}`)
+      req.body.user = req.user._id
+      req.body.userName = req.user.name
+      req.body.userAvatar = req.user.avatar
+
+      // Push the subdoc for the review
+      console.log('new review to be pushed', req.body)
+      resort.forEach(element => {
+        console.log('inside for Each', element.resortName)
+        element.reviews.push(req.body)
+        element.save(function (err) {})
+      })
     })
   })
+  res.redirect(`/resorts/${req.params.id}`)
 }
+
+// Always save the top-level document (not subdocs)
 
 module.exports = {
   create
